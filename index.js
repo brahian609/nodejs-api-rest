714761d5f89f59b87d512b315e20e1131c56cf10
Brahian Giraldo Medina <brahian609@hotmail.com>
2017-02-06 22:39:37 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+'use strict'
+
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+const port = process.env.PORT || 3001
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.get('/api/product', (req, res) => {
+    res.status(200).send({ products: [] })
+})
+
+app.get('/api/product/:productId', (req, res) => {
+
+})
+
+app.post('/api/product', (req, res) => {
+    console.log('req.body');
+    console.log(req.body);
+    res.status(200).send({ message: 'El producto se ha recibido' })
+})
+
+app.put('/api/product/:productId', (req, res) => {
+
+})
+
+app.delete('/api/product/:productId', (req, res) => {
+
+})
+
+app.listen(port, () => {
+    console.log(`Server started puerto ${port}`)
+})
